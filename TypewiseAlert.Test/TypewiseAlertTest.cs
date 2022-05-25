397e17614c15bce54e09ab27390b11ec3d01fd3d
Anitha-Bhat <anithabhat97@gmail.com>
2022-05-25 23:19:09 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -1,3 +1,4 @@
+using Moq;
 using System;
 using Xunit;
 
@@ -8,8 +9,34 @@ namespace TypewiseAlert.Test
     [Fact]
     public void InfersBreachAsPerLimits()
     {
-      Assert.True(TypewiseAlert.inferBreach(12, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
+      Assert.True(TypewiseAlert.inferBreach(12, 20, 30) is LowBreachType);
+      Assert.True(TypewiseAlert.inferBreach(35, 20, 30) is HighBreachType);
+      Assert.True(TypewiseAlert.inferBreach(25, 20, 30) is NormalBreachType);
+      Assert.True(TypewiseAlert.classifyTemperatureBreach(new PassiveCooling(), 50) is HighBreachType);
+      Assert.True(TypewiseAlert.classifyTemperatureBreach(new PassiveCooling(), 25) is NormalBreachType);
+      Assert.True(TypewiseAlert.classifyTemperatureBreach(new HighActiveCooling(), 50) is HighBreachType);
+      Assert.True(TypewiseAlert.classifyTemperatureBreach(new HighActiveCooling(), -1) is LowBreachType);
+      Assert.True(TypewiseAlert.classifyTemperatureBreach(new MediumActiveCooling(), 50) is HighBreachType);
+      Assert.True(TypewiseAlert.classifyTemperatureBreach(new MediumActiveCooling(), 30) is NormalBreachType);
+
+
+      var alertTarget = new Mock<AlertTarget>();
+      var batteryChar1 = new TypewiseAlert.BatteryCharacter() { coolingType = new PassiveCooling() };
+
+      TypewiseAlert.checkAndAlert(alertTarget.Object, batteryChar1, 25);
+      alertTarget.Verify(func => func.SendTarget(It.IsAny<NormalBreachType>()),Times.Exactly(1));
+
+      var batteryChar2 = new TypewiseAlert.BatteryCharacter() { coolingType = new HighActiveCooling() };
+
+      TypewiseAlert.checkAndAlert(alertTarget.Object, batteryChar2, 50);
+      alertTarget.Verify(func => func.SendTarget(It.IsAny<HighBreachType>()), Times.Exactly(1));
+
+      var batteryChar3 = new TypewiseAlert.BatteryCharacter() { coolingType = new HighActiveCooling() };
+
+      TypewiseAlert.checkAndAlert(alertTarget.Object, batteryChar3, -1);
+      alertTarget.Verify(func => func.SendTarget(It.IsAny<LowBreachType>()), Times.Exactly(1));
+
+
     }
   }
 }
