92a0a4bd213ddf990998417f7e518c9c4879b12f
Anitha-Bhat <anithabhat97@gmail.com>
2022-05-25 23:16:13 +0530

    Add files via upload

---
+++ TypewiseAlert/AlertTarget.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TypewiseAlert
+{
+  public interface IAlertTarget
+  {
+   void SendTarget(BreachType breachType);
+  }
+
+  public abstract class AlertTarget
+  {
+    public abstract void SendTarget(BreachType breachType);
+  }
+
+
+  public class Controller:AlertTarget
+  {
+    public override void SendTarget(BreachType breachType)
+    {
+      const ushort header = 0xfeed;
+      Console.WriteLine("{} : {}\n", header, breachType);
+    }
+      
+  }
+
+  public class Email : AlertTarget
+  {
+    public override void SendTarget(BreachType breachType)
+    {
+      string recepient = "a.b@c.com";
+      breachType.LogMessage(recepient);
+     
+    }
+  }
+}
---
+++ TypewiseAlert/BreachType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypewiseAlert
+{
+  public interface IBreachType
+  {
+    void LogMessage(string recepient);
+  }
+
+  public abstract class BreachType
+  {
+    public abstract void LogMessage(string recepient);
+  }
+
+  public class LowBreachType : BreachType
+  {
+    public override void LogMessage(string recepient)
+    {
+      Console.WriteLine("To: {}\n", recepient);
+      Console.WriteLine("Hi, the temperature is too low\n");
+    }
+  }
+
+  public class HighBreachType : BreachType
+  {
+    public override void LogMessage(string recepient)
+    {
+      Console.WriteLine("To: {}\n", recepient);
+      Console.WriteLine("Hi, the temperature is too high\n");
+    }
+  }
+  public class NormalBreachType : BreachType
+  {
+    public override void LogMessage(string recepient)
+    {
+     
+    }
+  }
+}
